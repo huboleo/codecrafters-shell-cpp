d1c4bd336e8f4ba41cc6734d4ab155abe182ada9
Hubert Trebicki <hubertrebicki@gmail.com>
2026-07-02 13:02:32 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <string>
 #include <print>
+#include <iostream>
 
 int main() {
-
     std::print("$ ");
+    std::string command;
+    std::getline(std::cin, command);
+    std::println("{}: command not found", command);    
 }
