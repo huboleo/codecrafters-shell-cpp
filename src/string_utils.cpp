59d85fcced306aa07a8188d51a2cb86d040a39ab
Hubert Trebicki <hubertrebicki@gmail.com>
2026-07-03 18:18:55 +0200

    codecrafters submit [skip ci]

--- src/string_utils.cpp
+++ src/string_utils.cpp
@@ -31,10 +31,13 @@ std::vector<std::string> string_utils::split_command(const std::string& input) {
     std::string current;
 
     bool in_single_quotes = false;
+    bool in_double_quotes = false;
 
     for (char character : input) {
-        if (character == '\'') {
+        if (character == '\'' && !in_double_quotes) {
             in_single_quotes = !in_single_quotes;
+        } else if (character == '\"' && !in_single_quotes) {
+            in_double_quotes = !in_double_quotes;
         } else if (std::isspace(static_cast<unsigned char>(character)) && !in_single_quotes) {
             if (!current.empty()) {
                 parts.push_back(current);
